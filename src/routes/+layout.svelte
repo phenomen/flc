2e3fe8d38425738007a28fbaa2303fcaec024a90
Aleksandr Vasilenko <phenomen@proton.me>
2023-03-21 16:13:24 -0500

    Squashed commit of the following:
    commit e41ee38d9bf2e64a4d2e8abc7d22a884c4eae624
    Author: Aleksandr Vasilenko <phenomen@proton.me>
    Date:   Tue Mar 21 16:11:42 2023 -0500
    
        Skip server check, Open in fullscreen
    
    commit 71eb7ea58c39ebb4847ec376d142af4c571d9ab2
    Author: phenomen <phenomen@proton.me>
    Date:   Sun Mar 5 17:17:10 2023 -0600
    
        Update CHANGELOG.md
    
    commit b9f6b78f71d7ad48020244d70dd026fc1c7c1f33
    Author: phenomen <phenomen@proton.me>
    Date:   Sun Mar 5 17:15:49 2023 -0600
    
        Zod IP validation, style tweaks
    
    commit 7512348799ca693fc7c801c72ebd7d704c595462
    Author: Aleksandr Vasilenko <phenomen@proton.me>
    Date:   Fri Mar 3 10:32:37 2023 -0600
    
        Optimization and types improvement
    
    commit 81484dfa48fb29563c631e0fa29a1845832caab3
    Author: Aleksandr Vasilenko <phenomen@proton.me>
    Date:   Thu Mar 2 12:13:28 2023 -0600
    
        Extra Zod
    
    commit d2371f215cd89c2b17f598eb918fde537ff717a0
    Author: Aleksandr Vasilenko <phenomen@proton.me>
    Date:   Thu Mar 2 12:01:21 2023 -0600
    
        Extra Zod
    
    commit 28e9999e7b19aa05f3a5a47aacc55b9de47d3dbf
    Author: Aleksandr Vasilenko <phenomen@proton.me>
    Date:   Thu Mar 2 11:39:58 2023 -0600
    
        Refactoring

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
   import Footer from "$lib/Footer.svelte";
 </script>
 
-<main class="max-w-xl py-8 px-2 mx-auto w-full h-full flex flex-col">
+<main class="max-w-xl py-6 px-2 mx-auto w-full h-full flex flex-col">
   <Header />
   <div class="flex flex-col flex-1">
     <slot />
