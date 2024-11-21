d822edf0ce063489fa9aea5e55af01278b13612e
Aleksandr Vasilenko <phenomen@pm.me>
2024-11-21 12:19:43 -0600

    Added support for SharedArrayBuffer; Fixed multiple windows of the same server

--- src/lib/scripts/webview.svelte.ts
+++ src/lib/scripts/webview.svelte.ts
@@ -1,8 +1,8 @@
 import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
-import { toast } from "svelte-sonner";
+import { nanoid } from "nanoid";
 
 export async function openWebview(url: string, id: string, title: string) {
-	const webview = new WebviewWindow(`foundry-${id}`, {
+	const webview = new WebviewWindow(`foundry-${id}-${nanoid()}`, {
 		title: `Foundry VTT - ${title}`,
 		url,
 		x: 0,
